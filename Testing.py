7e9922c35f481a35a673333e8ec699221d46eeea
Lennart Wisbar <lewisbar.git@gmail.com>
2019-06-09 00:42:53 +0200

    Commented some other debugging lines
    To test exporting from Pythonista

--- Testing.py
+++ Testing.py
@@ -1,11 +1,11 @@
 from world import *
 
 w = World()
-# w._map[7][2].is_explored = True
-for y, row in enumerate(w._map):
-	for x, room in enumerate(row):
-		if w.has_explored_neighbors(x, y):
-			print(x, y)
+w._map[7][2].is_explored = True
+# for y, row in enumerate(w._map):
+# 	for x, room in enumerate(row):
+# 		if w.has_explored_neighbors(x, y):
+# 			print(x, y)
 
 print(w.map_str(x=2, y=5, mode='outline'))
 print(w.map_str(x=2, y=5, mode='reveal'))
